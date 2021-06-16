0d3d26b6e213c80d054c357a8e16ddd8ae6fa9a7
hosimiya7 <nerokarurin@gmail.com>
2021-06-16 11:49:02 +0900

    function completed

--- index.js
+++ index.js
@@ -12,7 +12,33 @@ let paddleWidth = 75;
 let paddleX = (canvas.width-paddleWidth)/2;
 let rightPressed = false;
 let leftPressed = false;
+let brickRowCount = 3;
+let brickColumnCount = 5;
+let brickWidth = 75;
+let brickHeight = 20;
+let brickPadding = 10;
+let brickOffsetTop = 30;
+let brickOffsetLeft = 30;
+let score = 0;
 
+/**
+ * ブロックの位置情報
+ * @var Object[]
+ */
+let bricks = [];
+for(let c=0; c<brickColumnCount; c++) {
+    bricks[c] = [];
+    for(let r=0; r<brickRowCount; r++) {
+        bricks[c][r] = { x: 0, y: 0, status: 1 };
+    }
+}
+// let r = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+// let g = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+// let b = Math.floor( Math.random() * (max + 1 - min) ) + min ;
+
+/**
+ * ボールの描画
+ */
 function drawBall() {
     ctx.beginPath();
     ctx.arc(x, y, ballRadius, 0, Math.PI*2);
@@ -22,20 +48,56 @@ function drawBall() {
     x += dx;
     y += dy;
 }
+/**
+ * パドルの描画
+ */
 function drawPaddle() {
     ctx.beginPath();
     ctx.rect(paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
     ctx.fillStyle = "#0095DD";
     ctx.fill();
     ctx.closePath();
+    collisionDetection()
 }
-
+/**
+ * ブロックの描画
+ */
+function drawBricks() {
+    for(var c=0; c<brickColumnCount; c++) {
+        for(var r=0; r<brickRowCount; r++) {
+            if(bricks[c][r].status == 1) {
+                var brickX = (c*(brickWidth+brickPadding))+brickOffsetLeft;
+                var brickY = (r*(brickHeight+brickPadding))+brickOffsetTop;
+                bricks[c][r].x = brickX;
+                bricks[c][r].y = brickY;
+                ctx.beginPath();
+                ctx.rect(brickX, brickY, brickWidth, brickHeight);
+                ctx.fillStyle = "#0095DD";
+                ctx.fill();
+                ctx.closePath();
+            }
+        }
+    }
+}
+/**
+ * 描画する
+ */
 function draw() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
+    drawBricks();
     drawBall();
     drawPaddle();
-    if(y + dy > canvas.height - ballRadius || y + dy < ballRadius) {
+    drawScore();
+    if(y + dy < ballRadius) {
         dy = -dy;
+    } else if(y + dy > canvas.height-ballRadius) {
+        if(x > paddleX && x < paddleX + paddleWidth) {
+            dy = -dy;
+        }
+        else {
+            alert("GAME OVER");
+            document.location.reload();
+        }
     }
     if(x + dx > canvas.width - ballRadius || x + dx < ballRadius) {
         dx = -dx;
@@ -47,8 +109,14 @@ function draw() {
         paddleX -= 7;
     }
 }
+
+
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
+/**
+ * パドルが動く
+ * @param {Event} e 押されたキー情報
+ */
 function keyDownHandler(e) {
     if(e.key === "Right" || e.key === "ArrowRight") {
         rightPressed = true;
@@ -57,6 +125,10 @@ function keyDownHandler(e) {
         leftPressed = true;
     }
 }
+/**
+ * パドルが動いた後に止まる
+ * @param {Event} e 押されたキー情報
+ */
 function keyUpHandler(e) {
     if(e.key === "Right" || e.key === "ArrowRight") {
         rightPressed = false;
@@ -65,4 +137,45 @@ function keyUpHandler(e) {
         leftPressed = false;
     }
 }
-setInterval(draw, 10);
+
+document.addEventListener("mousemove", mouseMoveHandler, false);
+/**
+ * マウス操作
+ */
+ function mouseMoveHandler(e) {
+    let relativeX = e.clientX - canvas.offsetLeft;
+    if(relativeX > 0 && relativeX < canvas.width) {
+        paddleX = relativeX - paddleWidth/2;
+    }
+}
+/**
+ * 衝突検知
+ */
+function collisionDetection() {
+    for(let c=0; c<brickColumnCount; c++) {
+        for(let r=0; r<brickRowCount; r++) {
+            let b = bricks[c][r];
+            if(b.status == 1) {
+                if(x > b.x && x < b.x+brickWidth && y > b.y && y < b.y+brickHeight) {
+                    dy = -dy;
+                    b.status = 0;
+                    score++;
+                    if(score == brickRowCount*brickColumnCount) {
+                        alert("すごい！！！！大変よくできました！！");
+                        document.location.reload();
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * スコア描画
+ */
+function drawScore() {
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Score: " + score, 8, 20);
+}
+let interval = setInterval(draw, 10);
